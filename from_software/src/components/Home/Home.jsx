6084937b028dcf813049e192a377721b4c97a4f5
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2023-10-20 18:48:17 +0600

    work

--- from_software/src/components/Home/Home.jsx
+++ from_software/src/components/Home/Home.jsx
@@ -1,12 +1,22 @@
 import "./Home.scss";
 
-
-
-import Products from "./Products/Product/Products";
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
+import Products from "../Products/Products";
 const Home = () => {
     return (
         <div className="banner">
             <img src="https://dispatch.barnesandnoble.com/content/dam/ccr/homepage/daily/2023/09/27/27677_Quote_A1_DiscoverShortlistAnnouncement_09-27.jpg" alt="banner"></img>
+
+            <div className="main-content">
+
+                <div className="layout">
+                    <Category/>
+                    <Products/>
+
+
+                </div>
+            </div>
         </div>)
     ;
 };
