70cc752a3a8a8f98bcde242d5f3a7071f864ac9d
Yrnar <ernar030913@gmail.com>
2023-10-21 03:03:17 +0600

    cart

--- from_software/src/components/Header/Header.jsx
+++ from_software/src/components/Header/Header.jsx
@@ -1,25 +1,52 @@
+import { useEffect, useState } from "react";
+
 import { TbSearch } from "react-icons/tb"
 import { AiOutlineHeart } from "react-icons/ai"
 import { CgShoppingCart } from "react-icons/cg"
 
+import Cart from "../Cart/Cart";
+
 import "./Header.scss";
+import React from "react";
 
 const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const handleScroll = ()=>{
+        const offset = window.scrollY
+        if(offset>200){
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener("scroll", handleScroll)
+    }, [])
+    const [showCart, setShowCart] = useState(false);
     return (
-    <header className="main-header">
-        <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>Categories</li>
-            </ul>
-            <div className="center">BookStore</div>
-            <div className="right">
-                <TbSearch />
-                <AiOutlineHeart />
-                <CgShoppingCart />
-            </div>
-        </div>
-    </header>
+        <>
+            <header className={`main-header ${scrolled? 'sticky-header' : ''}` }>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center">BookStore</div>
+                    <div className="right">
+                        <TbSearch />
+                        <AiOutlineHeart />
+                        <span 
+                            className="cart-icon" 
+                            onClick={()=>setShowCart(true)}
+                        >
+                            <CgShoppingCart />
+                            {/* <span>5</span> */}
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showCart && <Cart setShowCart={setShowCart}/>}
+        </>
     );
 };
 
--- from_software/src/components/Home/Home.jsx
+++ from_software/src/components/Home/Home.jsx
@@ -1,20 +1,16 @@
 import "./Home.scss";
 
-//import Banner from "./Banner/Banner";
 import Category from "../Category/Category";
 import Products from "../Products/Products";
 const Home = () => {
     return (
-        <div className="banner">
+        <div className="home">
             <img src="https://dispatch.barnesandnoble.com/content/dam/ccr/homepage/daily/2023/09/27/27677_Quote_A1_DiscoverShortlistAnnouncement_09-27.jpg" alt="banner"></img>
 
             <div className="main-content">
-
                 <div className="layout">
-                <Category/>
-                <Products/>
-
-
+                    <Category/>
+                    <Products/>
                 </div>
             </div>
         </div>)
