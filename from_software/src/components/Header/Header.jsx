630dfac47caac347b41930590318aba06396a686
Madiyar <Shadvence@gmail.com>
2023-12-17 11:33:58 +0600

    api

--- from_software/src/components/Header/Header.jsx
+++ from_software/src/components/Header/Header.jsx
@@ -33,7 +33,6 @@ const Header = () => {
                 <div className="header-content">
                     <ul className="left">
                         <li><Link to="/" className="router-link">Home</Link></li>
-                        <li><Link to="/products/:id" className="router-link">Products</Link></li>
                         <li><Link to="/category/1" className="router-link">Category</Link></li>
                     </ul>
                     <div className="center"><Link to="/" className="router-link">BookStore</Link></div>
