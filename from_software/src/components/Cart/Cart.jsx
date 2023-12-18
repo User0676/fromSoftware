1fc7691746b7c41062ebc3e79549add0182854fb
Madiyar <Shadvence@gmail.com>
2023-12-18 18:47:04 +0600

    added checkout

--- from_software/src/components/Cart/Cart.jsx
+++ from_software/src/components/Cart/Cart.jsx
@@ -3,11 +3,34 @@ import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 
 import CartItem from "./CartItem/CartItem"
-
 import "./Cart.scss";
 import { Context } from "../../utils/context";
-const Cart =({setShowCart})=>{
+import { makePaymentRequest } from "../../utils/api";
+
+import { loadStripe } from "@stripe/stripe-js";
+
+
+const Cart = ({ setShowCart }) => {
     const {cartItems, cartSubTotal} = useContext(Context)
+
+    const stripePromise = loadStripe(
+        process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+    );
+
+    const handlePayment = async () => {
+        try {
+            const stripe = await stripePromise;
+            const res = await makePaymentRequest.post("/api/orders", {
+                products: cartItems,
+            });
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="cart-panel">
             <div className="opac-layer"></div>
@@ -37,7 +60,9 @@ const Cart =({setShowCart})=>{
                             <span className="text total">{cartSubTotal}</span>
                         </div>
                         <div className="button">
-                            <button className="checkout-cta">Checkout</button>
+                            <button className="checkout-cta" onClick={handlePayment}>
+                                Checkout
+                            </button>
                         </div>
                     </div>
                 </>}
